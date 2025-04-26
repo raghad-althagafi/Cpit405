da90dc2f175fd4397cf5b6d8942e72f653746762
Raghad_althagafi <ralthagafi0041@stu.kau.edu.sa>
2025-04-26 18:47:14 +0300

    services component

---
+++ src/ServicesPage/Services.js
@@ -0,0 +1,46 @@
+import './Services_Style.css';
+import Footer from '../Footer';
+import Header from '../Header';
+function Services() {
+    return (
+
+
+        <div className="page">
+
+        
+      
+        
+        <section className="services-section">
+          <h1 className="main-title">Our Services</h1>
+          <p className='intro-text'> At IT Solutions, we've designed services to address all issues,
+          <br></br>providing reliable, scalable, and secure IT solutions tailored to your business needs.</p>
+
+          
+          <button>Let's Talk</button>
+        
+      
+         
+        {/*   <div className="service-box">
+            <h2 className="service-title">Service 1</h2>
+            <p className="service-desc">وصف الخدمة الأولى.</p>
+          </div>
+      
+          <div className="service-box">
+            <h2 className="service-title">Service 2</h2>
+            <p className="service-desc">وصف الخدمة الثانية.</p>
+          </div>
+      
+          <div className="service-box">
+            <h2 className="service-title">Service 3</h2>
+            <p className="service-desc">وصف الخدمة الثالثة.</p>
+          </div> */}
+      
+        </section>
+      
+      </div>
+      
+      );
+    };
+    
+
+export default Services;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import HomePage from './HomePage/HomePage';
+import Services from './ServicesPage/Services';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HomePage />
+    {/* <HomePage /> */}
+    <Services />
   </React.StrictMode>
 );
 
