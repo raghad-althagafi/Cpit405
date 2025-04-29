d934247ef8ba46eff9b28dc3546d388b3de24f7c
sherihanalshareef <120980198+sherihanalshareef@users.noreply.github.com>
2025-04-29 22:17:08 +0300

    Add files via upload

--- src/AboutUs/server.js
+++ src/AboutUs/server.js
@@ -1,1 +1,18 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3000;
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Routes
+app.get(['/', '/about'], (req, res) => {
+    res.render('about', { title: 'About Us' });
+});
+
+
+app.get('/services', (req, res) => res.send('<h1>Services Page Coming Soon</h1>'));
+app.get('/contact', (req, res) => res.send('<h1>Contact Us Page Coming Soon</h1>'));
+
+app.listen(port, () => console.log(`Server running at http://localhost:${port}`));
